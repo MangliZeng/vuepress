569020f7caa883bba1f3acc94bf60683598b96ed
zml <zml@zmldeMacBook-Pro.local>
2022-08-21 21:01:11 +0800

    deploy

---
+++ assets/js/2.abebb071.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{229:function(i,n,s){"use strict";s.r(n);var t={props:{origin:String}},o=s(13),r=Object(o.a)(t,(function(){return(0,this._self._c)("div",[this._v("我是组件 "+this._s(this.origin))])}),[],!1,null,null,null);n.default=r.exports}}]);
